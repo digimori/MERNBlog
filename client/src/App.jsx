ca26c685286801585ecb69c4124f79a747c7d53a
digimori <hanaorikuu@gmail.com>
2024-01-03 20:07:38 +0000

    React-router Outlet added to manage page and link renders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar.component";
 
 const App = () => {
-    return (
-        <Navbar />
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Sign in page</h1>} />
+        <Route path="signup" element={<h1>Sign up page</h1>} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
