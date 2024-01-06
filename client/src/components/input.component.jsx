260b5cd4d34c3c0a393150d1a7bf1a9d24fd9047
digimori <hanaorikuu@gmail.com>
2024-01-06 20:33:01 +0000

    User auth form created - UI only,  Not linked to backend.

---
+++ client/src/components/input.component.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const InputBox = ({ name, type, id, value, placeholder, icon }) => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  return (
+    <div className="relative w-[100%] mb-4">
+      <input
+        type={
+          type == "password" ? (passwordVisible ? "text" : "password") : type
+        }
+        name={name}
+        id={id}
+        placeholder={placeholder}
+        defaultValue={value}
+        className="input-box"
+      />
+      <i className={"fi " + icon + " input-icon"}></i>
+
+      {type == "password" ? (
+        <i
+          className={
+            "fi fi-rr-eye" +
+            (!passwordVisible ? "-crossed" : "") +
+            " input-icon left-[auto] right-4 cursor-pointer"
+          }
+          onClick={() => setPasswordVisible((currentValue) => !currentValue)}
+        ></i>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default InputBox;
